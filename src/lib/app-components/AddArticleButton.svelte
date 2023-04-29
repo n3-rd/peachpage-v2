2ab0d483893a886bd7295cc58c9ecbca16ea6df3
n3-rd <godwinjdayo@gmail.com>
2023-04-29 21:26:23 +0100

    added add article modal

--- src/lib/app-components/AddArticleButton.svelte
+++ src/lib/app-components/AddArticleButton.svelte
@@ -1,5 +1,17 @@
-<div class="h-[4rem] w-[4rem] bg-[#0f35ff] p-4 rounded-full cursor-pointer">
+<script>
+	import { openModal } from 'svelte-modals';
+	import Modal from './Modal.svelte';
+
+	function handleClick() {
+		openModal(Modal, { title: 'Article Link' });
+	}
+</script>
+
+<button
+	class="h-[4rem] w-[4rem] bg-[#0f35ff] p-4 rounded-full cursor-pointer"
+	on:click={handleClick}
+>
 	<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"
 		><path fill="#ffffff" d="M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z" /></svg
 	>
-</div>
+</button>
